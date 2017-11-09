92b5ae10008a52cd123dd4379406dbb23d5d0b58
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-11-09 19:00:21 +0100

    Botón atras elimina Fragments

--- app/src/main/java/mviel/fragments/MainActivity.java
+++ app/src/main/java/mviel/fragments/MainActivity.java
@@ -1,10 +1,13 @@
 package mviel.fragments;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements Fragment2.OnFragmentInteractionListener2,Fragment3.OnFragmentInteractionListener3 {
 
@@ -44,8 +47,16 @@ public class MainActivity extends Activity implements Fragment2.OnFragmentIntera
 
     @Override
     public boolean estaFragment3EnActivity() {
-       Fragment3 f3= (Fragment3)getFragmentManager().findFragmentById(R.id.canto_inferior_dret);
+        Fragment3 f3= (Fragment3)getFragmentManager().findFragmentById(R.id.canto_inferior_dret);
         if(f3==null)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean estaFragment2EnActivity() {
+        Fragment2 f2= (Fragment2)getFragmentManager().findFragmentById(R.id.canto_superior_dret);
+        if(f2==null)
             return false;
         else
             return true;
@@ -54,5 +65,32 @@ public class MainActivity extends Activity implements Fragment2.OnFragmentIntera
     @Override
     public void onFragmentInteraction3(Uri uri) {
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentTransaction ft;
+        FragmentManager fm;
+        fm = getFragmentManager();
+        ft = fm.beginTransaction();
+
+        if (estaFragment3EnActivity()){
+            ft.remove(getFragmentManager().findFragmentById(R.id.canto_inferior_dret));
+            ft.commit();
+
+            mostrarToast("Ha desaparecido el Fragment 3");
+        } else if (estaFragment2EnActivity()){
+            ft.remove(getFragmentManager().findFragmentById(R.id.canto_superior_dret));
+            ft.commit();
+
+            mostrarToast("Ha desaparecido el Fragment 2");
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    public void mostrarToast(String mensaje){
+        Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG).show();
     }
 }
